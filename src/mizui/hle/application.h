408d9407a5df054d24f59229edc2d9fb76bc7476
Correia <gabriel_correia0202@outlook.com>
2024-08-22 11:03:16 -0400

    (HLE), Fixes a failure when checking the file type

--- src/mizui/hle/application.h
+++ src/mizui/hle/application.h
@@ -8,6 +8,7 @@ namespace mizui::hle {
     struct Application {
         u64 playId;
         u64 handle;
+        vfs::Path ios;
         std::string title;
         std::vector<u8> icon;
     };
--- src/mizui/hle/virtual_nx.cpp
+++ src/mizui/hle/virtual_nx.cpp
@@ -5,28 +5,26 @@
 #include <hle/virtual_nx.h>
 namespace mizui::hle {
     void VirtualNx::stockEveryExecutable(const std::vector<vfs::Path>& executables) {
-        for (const auto& [idx, path] :
-            std::views::enumerate(executables)) {
+        for (const auto& [idx, path] : std::views::enumerate(executables)) {
             const std::filesystem::path& local{path};
             bool duplicate{};
 #if defined(__linux__)
             struct stat64 ioStatus{};
             stat64(local.c_str(), &ioStatus);
-            if (ioStatus.st_mode & S_IFLNK) {
+            if (S_ISLNK(ioStatus.st_mode)) {
                 std::vector<char> followLink(100);
                 readlink(local.c_str(), &followLink[0], followLink.size());
                 stat64(&followLink[0], &ioStatus);
             }
-
+#endif
             for (const auto& unique : playable) {
                 if (unique.handle == ioStatus.st_ino)
                     duplicate = true;
                 if (duplicate)
                     break;
             }
-#endif
             if (!duplicate)
-                playable.emplace_back(idx);
+                playable.emplace_back(idx, ioStatus.st_ino, path);
         }
     }
 }
